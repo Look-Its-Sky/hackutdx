37246db1f892bcd29e2865ac72f7fdf3f6bb945f
Jude Joubert <joubertjude@gmail.com>
2024-06-25 12:13:01 -0500

    Add API support

--- api/main.py
+++ api/main.py
@@ -4,6 +4,11 @@ app = flask.Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Hello, World!'
+    response = flask.jsonify({
+        'msg' : 'Hello, World!'
+    })
+    response.headers.add('Access-Control-Allow-Origin', '*')
+
+    return response 
 
 app.run(host='0.0.0.0', port=8000)
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,25 @@
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const HOST = 'localhost'
+const PORT = 8000;
+
 function App() {
+  const [data, setData] = useState(null);    
+
+  useEffect(() => {
+    fetch(`http://${HOST}:${PORT}`)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error(`Network response was not ok\nresponse : ${response}`);
+        }
+        return response.json();
+      })
+      .then(data => setData(data))
+      .catch(error => console.error('Error:', error));
+    }, []);
+  
   return (
     <div className="App">
       <header className="App-header">
@@ -17,6 +35,14 @@ function App() {
         >
           Learn React
         </a>
+        {data && (
+          <div>
+            <h2>
+              API Test:{"\n"} 
+              <pre>{JSON.stringify(data)}</pre>
+            </h2>
+          </div>
+        )}
       </header>
     </div>
   );
