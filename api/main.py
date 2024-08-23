09a536688624e2b273711d46d84504f321c6ef5f
Jude Joubert <joubertjude@gmail.com>
2024-08-22 23:22:49 -0500

    Fixed CORS

--- api/main.py
+++ api/main.py
@@ -1,5 +1,6 @@
-import flask
+import flask, os
 
+DEBUG = os.getenv('DEBUG')
 app = flask.Flask(__name__)
 
 @app.route('/')
@@ -7,8 +8,18 @@ def index():
     response = flask.jsonify({
         'msg' : 'Hello, World!'
     })
-    response.headers.add('Access-Control-Allow-Origin', '*')
 
     return response 
 
-app.run(host='0.0.0.0', port=8000)
+@app.after_request
+def add_headers(response):
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    response.headers.add('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE')
+    response.headers.add('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+    response.headers.add('Access-Control-Allow-Credentials', 'true')
+
+    return response
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8000, debug=DEBUG)
+
